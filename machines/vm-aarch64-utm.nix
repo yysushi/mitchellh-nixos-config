df41fe305381932a777a77186b7d90ca426591c8
yysushi <yoshihiro.tsuji2@gmail.com>
2024-06-30 23:31:52 -0700

    enable qemu guest

--- machines/vm-aarch64-utm.nix
+++ machines/vm-aarch64-utm.nix
@@ -9,6 +9,7 @@
 
   # Qemu
   services.spice-vdagentd.enable = true;
+  services.qemuGuest.enable = true;
 
   # For now, we need this since hardware acceleration does not work.
   environment.variables.LIBGL_ALWAYS_SOFTWARE = "1";
